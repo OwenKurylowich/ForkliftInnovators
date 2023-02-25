b102c2a456ea8e820ac4ff11d967a395da8df87c
Potato7437 <122329795+Potato7437@users.noreply.github.com>
2023-02-25 12:53:05 -0500

    Update autonomousCommand.java

--- src/main/java/frc/robot/Commands/autonomousCommand.java
+++ src/main/java/frc/robot/Commands/autonomousCommand.java
@@ -49,47 +49,47 @@ public class autonomousCommand extends CommandBase {
     secondStop = true;
     secondDriveRun = true;
     thirdStop = true;
-    //Timer.delay(5); //delay to lean charge station in testing
+    Timer.delay(5); //delay to lean charge station in testing
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //if(firstDriveRun){
-      while(subsystem.autoDrive(1)){} //drive 9 feet
-      //firstDriveRun = false;
-    //}
+    if(firstDriveRun){
+      while(subsystem.autoDrive(1)){} //drive 9 feet//changed to 2 feet
+      firstDriveRun = false;
+    }
     
-    // if(resetEncoderOne){subsystem.resetEncoders();}
-    // if(getOffBal){
-    //   while(subsystem.autoOffBal()){} // get level on ground so it is out of community
-    //   getOffBal = false;
-    // }
-    // if(firstStop){
-    //   subsystem.drive(0.0,0.0); //small pause
-    //   Timer.delay(0.1); 
-    //   firstStop = false;
-    // }
-    // if(lineUp){
-    //   subsystem.turnToPoint(startYaw); //line up to point straight again
-    //   lineUp = false;
-    // }
-    // if(secondStop){
-    //   subsystem.drive(0.0,0.0);
-    //   Timer.delay(0.1); //pause again
-    //   secondStop = false;
-    // }
-    // if(secondDriveRun){
-    //   subsystem.autoDrive(-2.5); //drive back up the charge station
-    //   secondDriveRun = false;
-    // }
-    // if(thirdStop){
-    //   subsystem.drive(-0.05,-0.05);
-    //   Timer.delay(0.1); //pause again
-    //   thirdStop = false;
-    // }
-    // subsystem.toggleBalancePID(); //toggle balance to set some variables for balance
-    // while(subsystem.autoBal()){SmartDashboard.putNumber("NavX Pitch",navx.getPitch());} //balance
+     if(resetEncoderOne){subsystem.resetEncoders();}
+     if(getOffBal){
+       while(subsystem.autoOffBal()){} // get level on ground so it is out of community
+       getOffBal = false;
+     }
+     if(firstStop){
+      subsystem.drive(0.0,0.0); //small pause
+       Timer.delay(0.1); 
+       firstStop = false;
+     }
+     if(lineUp){
+       subsystem.turnToPoint(startYaw); //line up to point straight again
+       lineUp = false;
+     }
+     if(secondStop){
+       subsystem.drive(0.0,0.0);
+       Timer.delay(0.1); //pause again
+       secondStop = false;
+     }
+     if(secondDriveRun){
+       subsystem.autoDrive(-2.5); //drive back up the charge station//made 3.5 from 2.5
+       secondDriveRun = false;
+     }
+     if(thirdStop){
+       subsystem.drive(-0.05,-0.05);
+       Timer.delay(0.1); //pause again
+       thirdStop = false;
+     }
+     subsystem.toggleBalancePID(); //toggle balance to set some variables for balance
+     while(subsystem.autoBal()){SmartDashboard.putNumber("NavX Pitch",navx.getPitch());} //balance
   }
 
   // Called once the command ends or is interrupted.
